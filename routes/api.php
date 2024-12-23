0eae575464518bd923e8dc6ef8c2368cff1c1bba
Mariano <marianosalas24@gmail.com>
2024-12-23 11:12:21 -0300

    - Login

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\canchaController;
 use App\Http\Controllers\reservaController;
+use App\Http\Controllers\AuthController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -14,3 +15,5 @@ Route::post('/canchas', [canchaController::class, 'store']);
 
 Route::get('/reservas', [reservaController::class, 'index']);
 Route::post('/reservas', [reservaController::class, 'store']);
+
+Route::post('/login', [AuthController::class, 'login']);
