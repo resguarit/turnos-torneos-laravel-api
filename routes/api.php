3ff517ae27e4f8df81469e67a2e266d8dff724ee
Mariano <marianosalas24@gmail.com>
2024-12-26 10:57:33 -0300

    Endpoint /api/disponibilidad para obtener las fechas que tienen horarios no disponibles

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\canchaController;
 use App\Http\Controllers\reservaController;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\disponibilidadController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -15,5 +16,7 @@ Route::post('/canchas', [canchaController::class, 'store']);
 
 Route::get('/reservas', [reservaController::class, 'index']);
 Route::post('/reservas', [reservaController::class, 'store']);
+
+Route::get('/disponibilidad', [disponibilidadController::class, 'getHorariosNoDisponibles']);
 
 Route::post('/login', [AuthController::class, 'login']);
