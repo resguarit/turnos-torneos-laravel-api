a703eeceb6fefb4d0ba9841ab9f19eed2130c43d
naimguar <naimguar@gmail.com>
2025-03-06 09:17:11 -0300

    Update predis dependency version and enhance CORS configuration for Sanctum

--- config/cors.php
+++ config/cors.php
@@ -1,10 +1,12 @@
 <?php
+
 return [
-    'paths' => ['api/*'],
+    'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['*'], // Your Vite dev server URL
+    'allowed_origins' => ['http://localhost:5173', 'http://127.0.0.1:5173'], // Incluye ambas variantes
+    'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
     'max_age' => 0,
-    'supports_credentials' => true, // Change this to true if using credentials
+    'supports_credentials' => true, // Cámbialo a true si usas autenticación
 ];
