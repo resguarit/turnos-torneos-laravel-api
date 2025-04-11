091bb25ca7d02c8c2c2e0047d289a51ecacc5908
Mariano <marianosalas24@gmail.com>
2025-04-11 09:55:19 -0300

    Se actualizan los orígenes permitidos en la configuración CORS para incluir la URL del servidor de desarrollo de Vite y la URL de producción.

--- config/cors.php
+++ config/cors.php
@@ -2,7 +2,7 @@
 return [
     'paths' => ['api/*'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['*'], // Your Vite dev server URL
+    'allowed_origins' => ['https://rgturnos.com.ar', 'http://localhost:5173'], // Your Vite dev server URL
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
     'max_age' => 0,
