6c4be250bed90c5be72ee47ecca6805e915db4ae
Mariano <marianosalas24@gmail.com>
2025-05-07 10:43:08 -0300

    Mercado Pago controller

---
+++ app/Http/Controllers/Checkout/MercadoPagoController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\Checkout;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class MercadoPagoController extends Controller
+{
+    public function createPreference(Request $request)
+    {
+        
+    }
+}
