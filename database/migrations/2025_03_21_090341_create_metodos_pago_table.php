8464588f8343f74a26b5ef604b94a691063a864c
moragentil <moragentil@gmail.com>
2025-05-08 09:59:42 -0300

    Creacion de migracion de tabla sanciones

--- database/migrations/2025_03_21_090341_create_metodos_pago_table.php
+++ database/migrations/2025_03_21_090341_create_metodos_pago_table.php
@@ -4,7 +4,6 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 use App\Models\MetodoPago;
-use App\Models\MetodoPago;
 return new class extends Migration
 {
     /**
