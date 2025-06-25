26effd0c4ee407f5a9eda223b7a32526a8e8bf49
Mariano <marianosalas24@gmail.com>
2025-06-25 10:16:24 -0300

    cors para rockandgol

--- config/cors.php
+++ config/cors.php
@@ -3,7 +3,7 @@
 return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['https://rgturnos.com.ar', '*.localhost:5173', 'http://localhost:5173', 'https://51dd-191-84-236-181.ngrok-free.app/'], // Your Vite dev server URL
+    'allowed_origins' => ['https://rgturnos.com.ar', '*.localhost:5173', 'http://localhost:5173', 'https://rockandgol.com.ar', 'https://complejorockandgol.com.ar/'], // Your Vite dev server URL
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
     'max_age' => 0,
