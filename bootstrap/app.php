a1010079975d8e7f3c6829c80f08bccce93630cb
Mariano <marianosalas24@gmail.com>
2025-07-10 08:25:57 -0300

    Configuracion para multicomplejos. Middleware registrado en app, cors habilitado para todos los subdominios.

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,6 +4,7 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use App\Http\Middleware\CheckMercadoPagoEnabled;
+use App\Http\Middleware\SetTenantDatabase;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -16,6 +17,11 @@ return Application::configure(basePath: dirname(__DIR__))
         // Registrar el middleware de MercadoPago
         $middleware->alias([
             'check.mercadopago' => CheckMercadoPagoEnabled::class,
+        ]);
+
+        $middleware->group('api', [
+            SetTenantDatabase::class,
+            \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
--- config/cors.php
+++ config/cors.php
@@ -3,7 +3,7 @@
 return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['https://rgturnos.com.ar', '*.localhost:5173', 'http://localhost:5173', 'https://51dd-191-84-236-181.ngrok-free.app/'], // Your Vite dev server URL
+    'allowed_origins' => ['https://rgturnos.com.ar','*.rgrurnos.com.ar' ,'*.localhost:5173', 'http://localhost:5173'], // Your Vite dev server URL
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
     'max_age' => 0,
